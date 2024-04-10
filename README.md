489d75749d476f0205df284cb5d516d19da8183f
Rubin-khadka <rubin.robotic@gmail.com>
2024-04-10 17:41:17 +0545

    change to main file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # file-cypher
+
+## Project Description
---
+++ file_cypher.h
@@ -0,0 +1,13 @@
+/*
+    header file of file cypher project
+*/
+
+#ifndef FILE_CYPHER_H
+#define FILE_CYPHER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif /* FILE_CYPHER_H */
